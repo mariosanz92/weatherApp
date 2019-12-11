9df707b030fdea3430159644cf0ff7710508f04c
Sanz Martinez <mosn@gft.com>
2019-12-11 18:12:20 +0100

    added input to add new cities

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import LocationList from './components/WeatherLocation/LocationList';
+import TextField from '@material-ui/core/TextField';
 
-const cities = [
-  'Valencia,es',
-  'Washington,us',
-  'Tokio,jap',
-  'London,uk'
-]
 
-function App() {
-  return (
-    <div className="App">
-      <LocationList cities= {cities}/>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state= 
+    { city : '',
+      cities : [
+      'Valencia,es',
+      'Washington,us',
+      'Tokio,jap',
+    ]}
+  }
+  handleChange = event => {
+  this.setState({ city: event.target.value})
+  }
+
+  handleClick = () => {
+    this.setState({cities: [...this.state.cities, this.state.city]})
+
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <TextField className="" value={this.state.city} onChange={this.handleChange} />
+        <button onClick= {this.handleClick} >add city</button>
+        <LocationList cities= {this.state.cities}/>
+      </div>
+    )
+  }
 }
-
 export default App;
